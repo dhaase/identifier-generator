ed4dd6685bcc8b2e1418c8b8913f47fb4900faca
dhaase <dhaase@users.noreply.github.com>
2018-12-12 22:51:33 +0100

    initial import

--- src/main/java/eu/dirk/haase/identifier/IdentifierGenerator.java
+++ src/main/java/eu/dirk/haase/identifier/IdentifierGenerator.java
@@ -6,6 +6,4 @@ public interface IdentifierGenerator {
 
     long nextLong();
 
-    byte[] nextBytes();
-
 }
---
+++ src/test/java/eu/dirk/haase/identifier/SipHashInlineTest.java
@@ -0,0 +1,32 @@
+package eu.dirk.haase.identifier;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+import java.math.BigInteger;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class SipHashInlineTest {
+
+    @Test
+    public void test() {
+        final long val = 0xFAFAFAFAFAFAFAFAL;
+        BigInteger value = BigInteger.valueOf(val);
+        final byte[] data = value.toByteArray();
+
+        System.out.println("A: " + data[4]);
+        System.out.println("B: " + nthByteOfLong(1, value.longValue()));
+
+        long hash2 = SipHashInline.hash24(1L,3L, value.longValue());
+        System.out.println("::" + hash2);
+        System.out.println("::" + 6340469858319189276L);
+    }
+
+    private static final long BYTE_MASK = 0xFFL;
+
+    private byte nthByteOfLong(int n, long number) {
+        return (byte) ((number >>> (8 * n)) & BYTE_MASK);
+    }
+
+}
