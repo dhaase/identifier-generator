2dd6202b0951219a1f51d2e8ee2f97ef0d53572a
dhaase <dhaase@users.noreply.github.com>
2018-12-22 19:26:25 +0100

    initial import

--- src/main/java/eu/dirk/haase/identifier/CompactIdentifierRepresentation.java
+++ src/main/java/eu/dirk/haase/identifier/CompactIdentifierRepresentation.java
@@ -6,11 +6,7 @@ package eu.dirk.haase.identifier;
  */
 public class CompactIdentifierRepresentation {
 
-
-    private final static char[] ALL_DIGITS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-            '$', '#'};
+    private static final char[] ALL_DIGITS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$#".toCharArray();
 
     private final static int MASK_6_BIT_I = 0b00000000000000000000000000111111;
 
